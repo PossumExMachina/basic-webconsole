2596914bb00b2919dd89c9171200a0cd7ef7c94d
johanakrocova <krocovajohana@gmail.com>
2023-12-14 17:24:08 +0100

    Removed unused imports

--- src/main/java/monitoring/appServer/application/URLService.java
+++ src/main/java/monitoring/appServer/application/URLService.java
@@ -1,5 +1,4 @@
 package monitoring.appServer.application;
-import monitoring.appServer.application.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
--- src/main/java/monitoring/serverResources/disk/DiskService.java
+++ src/main/java/monitoring/serverResources/disk/DiskService.java
@@ -1,18 +1,13 @@
 package monitoring.serverResources.disk;
-
 import monitoring.CommandExec;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class DiskService {
+
     @Autowired
     CommandExec commandExec;
 
