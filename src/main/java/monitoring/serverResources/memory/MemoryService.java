92e3f95ddec4541e55408e38528ede16275c5867
johanakrocova <krocovajohana@gmail.com>
2024-02-08 20:07:26 +0100

    fixed memory parsing

--- src/main/java/monitoring/serverResources/memory/MemoryService.java
+++ src/main/java/monitoring/serverResources/memory/MemoryService.java
@@ -7,11 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,16 +22,6 @@ public class MemoryService {
 
     private static final Logger logger = LoggerFactory.getLogger(DockerControlService.class);
 
-
-    /**
-     * Retrieves free memory information from the system.
-     *
-     * Executes a command to get free memory details and parses the output into a list of Memory objects.
-     * Throws an IOException if the command execution returns no output.
-     *
-     * @return List<Memory> A list of Memory objects representing the free memory information.
-     * @throws IOException If the command execution fails or returns no output.
-     */
     public List<Memory> getFreeMemory() throws IOException {
         List<String> outputLines = commandExec.executeCommand(commandStrategy.getFreeMemoryCmd());
         if (outputLines == null || outputLines.isEmpty()) {
@@ -47,32 +34,20 @@ public class MemoryService {
                 .collect(Collectors.toList());
     }
 
-
-    /**
-     * Parses a single line of memory usage command output into a Memory object.
-     *
-     * Uses regular expressions to match the expected format of the memory information.
-     * Logs an error and returns null if the line does not match the expected pattern.
-     *
-     * @param line A string representing a single line of memory usage output.
-     * @return Memory A Memory object populated with values parsed from the line, or null if parsing fails.
-     */
     private Memory parseLine(String line) {
-        Pattern pattern = Pattern.compile("([\\w]+)\\s+([0-9]+)\\s+([0-9]+)\\s+([0-9]+).*");
-        Matcher matcher = pattern.matcher(line);
+        String[] parts = line.trim().split("\\s+");
+        if (parts.length >= 3) {
+            String total = parts[0];
+            String used = parts[2];
+            String free = parts[4];
 
-        if (!matcher.matches()) {
-            logger.error("Could not match the pattern for line: {}", line);
-            return null; // or throw a custom exception
+            logger.info("Total: " + total + ", Used: " + used + ", Free: " + free);
+            return new Memory(total, used, free);
+        } else {
+            logger.error("Unexpected format for line: {}", line);
+            return null;
         }
-
-        String total = matcher.group(2);
-        String used = matcher.group(3);
-        String free = matcher.group(4);
-
-        return new Memory(total, used, free);
     }
 }
-
 
 
